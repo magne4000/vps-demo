d27c209fea4ff7dabeaa65dc47684b7018654f2d
Joel Charles <joel.charles91@gmail.com>
2026-05-14 12:47:45 +0200

    fix migration

--- database/kysely/migrations/001_create_todos_table.ts
+++ database/kysely/migrations/001_create_todos_table.ts
@@ -1,10 +1,13 @@
-import type { Kysely } from "kysely";
+import { type Kysely, SqliteAdapter } from "kysely";
 import type { Database } from "../types";
 
 export async function up(db: Kysely<Database>): Promise<void> {
+  const adapter = db.getExecutor().adapter;
+  const isSqlite = adapter instanceof SqliteAdapter;
+
   await db.schema
     .createTable("todos")
-    .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
+    .addColumn("id", isSqlite ? "integer" : "serial", (col) => col.primaryKey())
     .addColumn("text", "text", (col) => col.notNull())
     .execute();
 }
